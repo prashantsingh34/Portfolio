4c759f201f32bb4b248b21ca128e55896ef2b34f
prashantsingh34 <113302801+prashantsingh34@users.noreply.github.com>
2024-03-29 18:23:43 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AboutCard from './components/About/AboutCard';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<AboutCard />);
+  const linkElement = screen.getByText(/I am currently employed/i);
   expect(linkElement).toBeInTheDocument();
 });
