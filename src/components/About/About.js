6cc0f25c8c1b66cff46c342b73c789709a2007a5
prashantsingh34 <113302801+prashantsingh34@users.noreply.github.com>
2024-04-11 00:57:02 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -41,7 +41,7 @@ function About() {
         <Techstack />
 
         <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
+          <strong className="purple">Tools</strong>that  I use
         </h1>
         <Toolstack />
 
